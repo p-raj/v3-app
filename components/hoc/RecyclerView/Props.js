d725e647c08b85276dae23ef92e841cca4fe6fe0
Ankit Popli <ankitpopli1891@gmail.com>
2017-06-23 17:39:42 +0530

    Squashed 'src/v3-core/' changes from 1ca583b..413d575
    413d575 Merge commit 'eef90329456cf67d64d4acf4356eeb0f1ac0366c' into develop
    0a4f0db refactor: extract toDotNotation to utils package
    d9e5e0e refactor: remove unused files :fire:
    b934699 refactor: remove unused file :fire:
    
    git-subtree-dir: src/v3-core
    git-subtree-split: 413d57578d82f32c2c3a2ac37d410cba30b5bf9f

--- components/hoc/RecyclerView/Props.js
+++ components/hoc/RecyclerView/Props.js
@@ -19,22 +19,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Item from '../../dynamic/Item';
 import { withValues } from '../withValues';
+import { toDotNotation } from '../../../utils';
 
-const toDotNotation = (json = {}, prefix = undefined) => {
-    let obj = JSON.parse(JSON.stringify(json));
-    let dot = {};
-    for (let key of Object.keys(obj)) {
-        const k = prefix ? `${prefix}.${key}` : key;
-        if (typeof (obj[key]) === typeof ({})) {
-            dot = {
-                ...toDotNotation(obj[key], k)
-            };
-        } else {
-            dot[k] = obj[key];
-        }
-    }
-    return dot;
-};
 
 class WithValueItem extends React.Component {
     // noinspection JSUnusedGlobalSymbols
