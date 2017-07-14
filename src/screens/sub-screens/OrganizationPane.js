8e1e0bb80a9926da18fa072d7b21943c136f01fe
Ankit Popli <ankitpopli1891@gmail.com>
2017-07-14 21:20:21 +0530

    fix: position the applications at the top
     - on the web, this used to work fine since web layout doesn't take full
       space, on native it feels weird

--- src/screens/sub-screens/OrganizationPane.js
+++ src/screens/sub-screens/OrganizationPane.js
@@ -102,7 +102,7 @@ class OrganizationPane extends React.Component {
                             </Request.Start>
                             {!_.isEmpty(this.state[`application-${key}-data`]) &&
                             <Request.Success>
-                                <View style={[{flexGrow: 1, justifyContent: 'center'}]}>
+                                <View style={{flex: 1, alignContent: 'flex-start'}}>
                                     <SortableGrid
                                         blockTransitionDuration={ 400 }
                                         activeBlockCenteringDuration={ 200 }
