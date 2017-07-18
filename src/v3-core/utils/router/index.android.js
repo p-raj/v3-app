60d56e5dd91a9645bff066c9039fedfa25523441
Ankit Popli <ankitpopli1891@gmail.com>
2017-07-18 20:38:19 +0530

    refactor: remove obsolete code, use core Redirect component :fire:
     - the code used to override Redirect component functionality by making
       it listen to the push changes, but the component is intended to be
       used for single render & removal from the tree

--- src/v3-core/utils/router/index.android.js
+++ src/v3-core/utils/router/index.android.js
@@ -1,27 +1,7 @@
 import * as Routing from 'react-router-native';
 
-export default Routing;
+// export default Routing;
 export const Link = Routing.Link;
 export const Route = Routing.Route;
 export const Router = Routing.NativeRouter;
-
-const R = Routing.Redirect;
-const getConfig = ({push, from, to}) => {
-    return {
-        push, from, to
-    }
-};
-
-
-export class Redirect extends R {
-    shouldComponentUpdate(nextProps) {
-        // this should be slow as heck,
-        // will change the implementation asap once validated
-        return JSON.stringify(getConfig(this.props)) !== JSON.stringify(getConfig(nextProps));
-    }
-
-    componentDidUpdate() {
-        if (!this.isStatic())
-            this.perform()
-    }
-}
+export const Redirect = Routing.Redirect;
