de399958166a49c1dcbd977ee26777b73e5e7f38
Ankit Popli <ankitpopli1891@gmail.com>
2017-09-20 20:30:02 +0530

    refactor: use recompose for publishing APIs
     - text-area & text-input have simlar HOCs. alternative way of writing a
       higher order component

--- src/app/Template/Component/apis/text-area/index.js
+++ src/app/Template/Component/apis/text-area/index.js
@@ -1,42 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getDisplayName } from 'recompose';
+import {
+    compose,
+    getContext,
+    getDisplayName,
+    mapProps,
+    setDisplayName,
+    withHandlers
+} from 'recompose';
 
 
-export function withTextAreaProps(WrappedComponent) {
-    class WithTextAreaProps extends React.Component {
-        static contextTypes = {
+export const withTextAreaProps = (WrappedComponent) => {
+    return compose(
+        setDisplayName(`WithTextAreaProps(${getDisplayName(WrappedComponent)})`),
+        getContext({
             enqueue: PropTypes.func,
             setVariable: PropTypes.func,
-        };
+        }),
+        withHandlers({
+            onChangeText: props => text => {
+                props.setVariable(props.name, text);
 
-        render() {
-            // filter out extra props that are specific to this HOC and shouldn't be
-            // passed through
-            // eslint-disable-next-line
-            const {disabled, action, theme, value, placeholder, style} = this.props;
-
-            // Pass props to wrapped component
-            return (
-                <WrappedComponent
-                    onChangeText={this.onChangeText}
-                    editable={!disabled}
-                    defaultValue={value}
-                    placeholder={placeholder}
-                    style={style}/>
-            );
-        }
-
-        onChangeText = (text) => {
-            const props = this.props;
-            this.context.setVariable(props.name, text);
-
-            if (!this.props.action) return;
-
-            this.context.enqueue(this.props.action);
-        }
-    }
-
-    WithTextAreaProps.displayName = `WithTextAreaProps(${getDisplayName(WrappedComponent)})`;
-    return WithTextAreaProps;
-}
+                if (!props.action) return;
+                props.enqueue(props.action);
+            }
+        }),
+        mapProps(({disabled, value, ...rest}) => {
+            const {enqueue, setVariable, ...props} = rest;
+            return {
+                ...props,
+                editable: !disabled,
+                defaultValue: value
+            }
+        })
+    )((props) => <WrappedComponent {...props}/>);
+};
